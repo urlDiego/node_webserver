24910f4c97f50d6eb7bc78719dd184d07b7a2b77
urlDiego <diegoarmandogomezvasquez2003@gmail.com>
2024-07-16 15:19:13 -0600

    v0.0.1

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const { envs } = require('./config/env')
+const { startServer } = require ('./server/server')
+
+
+const main = () => {
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    })
+}
+
+( async () => {
+    main()
+}) ()
