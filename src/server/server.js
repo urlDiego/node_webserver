b9725945eeec9cab22ab397a45ed20c390172f16
urlDiego <diegoarmandogomezvasquez2003@gmail.com>
2024-07-23 14:41:27 -0600

    Importaciones y exportaciones tipo ES6

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import path from 'path'
 
-const startServer = (options) => {
+export const startServer = (options) => {
     const { port, public_path = 'public' } = options
     const app = express()
 
@@ -15,8 +15,4 @@ const startServer = (options) => {
     app.listen(port, () => {
         console.log(`Escuchando en el port ${port}`);
     })
-}
-
-module.exports = {
-    startServer
 }
